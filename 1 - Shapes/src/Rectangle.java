f00aa4f57fcc5006695acd2ce7dc2364372f10e3
Jay Bains <jayarjunbains@gmail.com>
2025-05-06 16:45:54 +0100

    Task 2, Calculate area and perimeter of shapes

---
+++ 1 - Shapes/src/Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle extends Shape{
+    private final double length;
+    private final double height;
+
+    public Rectangle(double length, double height){
+        this.length = length;
+        this.height = height;
+    }
+
+    @Override
+    public double perimeter() {
+        return length*2 + height*2;
+    }
+
+    @Override
+    public double area() {
+        return length*height;
+    }
+}
